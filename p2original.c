c520d18200552fb5a740240acec8c8e0e28f9f07
BHARATH2145 <94664858+Bharath2145@users.noreply.github.com>
2022-03-07 06:26:51 +0000

    codded

--- p2original.c
+++ p2original.c
@@ -1,46 +1,41 @@
-#include<stdio.h>
-struct_fraction
+ #include<stdio.h>
+struct _fraction
 {
-  int num, den;
+int num,den;
 };
-typedef struct_fraction Fraction;
-Fraction input_fraction()
+typedef struct _fraction
+fraction;
+fraction input_fraction()
 {
-  Fraction f:
+  fraction f;
   printf("enter the fraction\n");
-  scanf("%d %d",&f.num,&f.den);
+  scanf("%d%d",&f.num,&f.den);
   return f;
 }
-Fraction find smallest(Fraction f1,Fraction f2,Fraction f3)
+fraction find_smallest(fraction f1,fraction f2,fraction f3,fraction f4)
 {
   int common_denominator = f1.den * f2.den * f3.den;
-  int num1 = f1.num*common_denominator/f1.den;
-  int num2 = f2.num*common_denominator/f2.den;
-  int num3 = f3.num*common_denominator/f3.den;
-  if(num1<num2&&num1<num3)
-  {
-    return num1;
-  }
-  else if(num2<num3)
-  {
-    retun num2;
-  }
+  int num1 = f1.num * common_denominator/f1.den;
+  int num2 = f2.num * common_denominator/f2.den;
+  int num3 = f3.num * common_denominator/f3.den;
+  if (num1 < num2 && num1 < num3)
+    return f1;
+  else if(num2 <num3)
+    return f2;
   else
-  {
-    retun num3;
-  }
+    return f3;
 }
-void output(fraction f1,fraction f2,fraction f3,Fraction smallest)
+void output(fraction f1,fraction f2,fraction f3,fraction smallest)
 {
-  printf("the smallest of %d/%d,%d/%d,%d/%d is %d/%d\n",f1num,f1den,f2num,f2den,f3num,f3den,smallest.num,smallest.den);
+  printf("the smallest of %d/%d, %d/%d, %d/%d is %d/%d\n",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
 }
 int main()
 {
-  Fraction f1,f2,f3,smallest;
-  f1 = input();
-  f2 = input();
-  f3 = input();
-  smallest = smallest(f1,f2,f3);
-  output (f1,f2,f3, smallest);
+  fraction f1,f2,f3,f4,smallest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  smallest=find_smallest(f1,f2,f3,f4);
+  output(f1,f2,f3,smallest);
   return 0;
 }
